5b650575c1bdb133dea0e682674ed11cca9d2405
Muhammad Anwar Ismail <24051204161@mhs.unesa.ac.id>
2025-04-08 17:26:09 +0700

    Create slicknav-init.js

---
+++ assets/js/slicknav-init.js
@@ -0,0 +1,8 @@
+
+// SlickNav Initialization for mobile menu
+$(document).ready(function(){
+    $('.header__menu ul').slicknav({
+        prependTo: '#mobile-menu-wrap',
+        label: 'MENU'
+    });
+});
